8361e450730d7c48fc4dfdb246a45476a1f53bfe
Yogesh Patil <officialyogeshp@gmail.com>
2024-01-06 19:17:22 -0500

    Added: Server Actions (Form)

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,4 +1,19 @@
 import Link from 'next/link';
+import { prisma } from '../db';
+import { redirect } from 'next/navigation';
+
+async function createTodo(data: FormData) {
+  'use server';
+
+  const title = data.get('title')?.valueOf();
+
+  if (typeof title !== 'string' || title.length === 0) {
+    throw new Error('Invalid Title');
+  }
+
+  await prisma.todo.create({ data: { title, complete: false } });
+  redirect("/")
+}
 
 export default function Page() {
   return (
@@ -6,13 +21,13 @@ export default function Page() {
       <header className="flex justify-between items-center mb-4">
         <h1 className="text-2xl">New</h1>
       </header>
-      <form className="flex gap-2 flex-col">
+      <form action={createTodo} className="flex gap-2 flex-col">
         <input
           type="text"
           name="title"
           className="border border-slate-300 bg-transparent rounded px-2 py-1 outline-none focus-within:border-slate-100"
         />
-        <div className='flex gap-1 justify-end'>
+        <div className="flex gap-1 justify-end">
           <Link
             className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none"
             href=".."
